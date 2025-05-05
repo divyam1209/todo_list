0c35e0f84bef7f039961fd13639c93848906fbea
DIvyam Saini <153718864+divyam1209@users.noreply.github.com>
2025-05-05 20:19:23 +0530

    Update index.js
    index.js updated

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// In-memory task list
 let items = [];
 let nextId = 1;
 
-// Home route
 app.get("/", function (req, res) {
     res.render("list", { items: items });
 });
 
-// Add new task
 app.post("/", function (req, res) {
     const itemText = req.body.d1d1;
     if (itemText.trim() !== "") {
@@ -24,7 +21,6 @@ app.post("/", function (req, res) {
     res.redirect("/");
 });
 
-// Unified update/delete route
 app.post("/action", (req, res) => {
     const action = req.body.action;
     const ids = req.body.item
@@ -46,7 +42,6 @@ app.post("/action", (req, res) => {
     res.redirect("/");
 });
 
-// Start server
 app.listen(8000, function () {
     console.log("Server Started on port 8000");
 });
